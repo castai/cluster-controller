e79561bbae9a19ade295effbba6578dc62d0ab66
anjmao <andzej.maciusovic@gmail.com>
2022-03-30 14:20:27 +0300

    Update deploy script example

--- hack/remote/deploy.sh
+++ hack/remote/deploy.sh
@@ -5,6 +5,21 @@ set -e
 # Go to git repo root.
 cd "$(git rev-parse --show-toplevel)"
 
+if [ -z "$API_KEY" ]; then
+  echo "API_KEY environment variable is not defined"
+  exit 1
+fi
+
+if [ -z "$API_URL" ]; then
+  echo "API_URL environment variable is not defined"
+  exit 1
+fi
+
+if [ -z "$CLUSTER_ID" ]; then
+  echo "CLUSTER_ID environment variable is not defined"
+  exit 1
+fi
+
 # Build bo binary and push docker image.
 IMAGE_TAG=v0.0.1
 GOOS=linux GOARCH=amd64 go build -ldflags "-X main.Version=${IMAGE_TAG}" -o bin/castai-cluster-controller .
@@ -16,13 +31,13 @@ docker push "$DOCKER_IMAGE_REPO:$IMAGE_TAG"
 LOCAL_CHART_DIR=../gh-helm-charts/charts/castai-cluster-controller
 helm upgrade -i cluster-controller $LOCAL_CHART_DIR \
   -f ./hack/remote/values.yaml \
-  --set image.repository="$DOCKER_IMAGE_REPO" \
-  --set image.tag="$IMAGE_TAG" \
+  --set image.repository="${DOCKER_IMAGE_REPO}" \
+  --set image.tag="${IMAGE_TAG}" \
   --set aks.enabled=false \
   --set serviceAccount.create="true" \
-  --set castai.apiKey="your-api-key" \
-  --set castai.apiURL="your-api-url" \
-  --set castai.clusterID="your-cluster-id" \
+  --set castai.apiKey="${API_KEY}" \
+  --set castai.apiURL="${API_URL}" \
+  --set castai.clusterID="${CLUSTER_ID}" \
   --history-max=3 \
   -n castai-agent
 
