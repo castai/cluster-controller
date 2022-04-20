edcfd628b3ab9497b36dd6fc3a7deac6427c32ca
anjmao <andzej@cast.ai>
2022-04-20 13:51:56 +0300

    aks init data rework

--- hack/remote/setup.sh
+++ hack/remote/setup.sh
@@ -7,8 +7,5 @@ if [ -z "$DOCKER_SECRET_TMPL_PATH" ]; then
   exit 1
 fi
 
-helm repo add castai-helm https://castai.github.io/helm-charts
-helm repo update
-
 $DOCKER_SECRET_TMPL_PATH castai-agent | kubectl apply -f - -n castai-agent
 
