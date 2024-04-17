bd1f9f5fc6d7c6d9731ca63dbaef2f73c70d28c2
Lachezar Tsonov <lachezar@cast.ai>
2024-04-17 15:31:56 +0300

    Change kubernetes_helpers.go to wait pkg

--- actions/kubernetes_helpers.go
+++ actions/kubernetes_helpers.go
@@ -6,17 +6,19 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/cenkalti/backoff/v4"
 	"github.com/sirupsen/logrus"
 	v1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	apitypes "k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/strategicpatch"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
+
+	"github.com/castai/cluster-controller/internal/waitext"
 )
 
-func patchNode(ctx context.Context, clientset kubernetes.Interface, node *v1.Node, changeFn func(*v1.Node)) error {
+func patchNode(ctx context.Context, log logrus.FieldLogger, clientset kubernetes.Interface, node *v1.Node, changeFn func(*v1.Node)) error {
 	oldData, err := json.Marshal(node)
 	if err != nil {
 		return fmt.Errorf("marshaling old data: %w", err)
@@ -34,10 +36,14 @@ func patchNode(ctx context.Context, clientset kubernetes.Interface, node *v1.Nod
 		return fmt.Errorf("creating patch for node: %w", err)
 	}
 
-	err = backoff.Retry(func() error {
+	patchImpl := waitext.WithTransientRetriesCtx(func(ctx context.Context) error {
 		_, err = clientset.CoreV1().Nodes().Patch(ctx, node.Name, apitypes.StrategicMergePatchType, patch, metav1.PatchOptions{})
 		return err
-	}, defaultBackoff(ctx))
+	}, func(err error) {
+		log.Warnf("patch node, will retry: %v", err)
+	})
+
+	err = wait.ExponentialBackoffWithContext(ctx, defaultBackoff(), patchImpl)
 	if err != nil {
 		return fmt.Errorf("patching node: %w", err)
 	}
@@ -46,7 +52,7 @@ func patchNode(ctx context.Context, clientset kubernetes.Interface, node *v1.Nod
 }
 
 func patchNodeStatus(ctx context.Context, log logrus.FieldLogger, clientset kubernetes.Interface, name string, patch []byte) error {
-	err := backoff.Retry(func() error {
+	patchNodeImpl := waitext.WithTransientRetriesCtx(func(ctx context.Context) error {
 		_, err := clientset.CoreV1().Nodes().PatchStatus(ctx, name, patch)
 		if k8serrors.IsForbidden(err) {
 			// permissions might be of older version that can't patch node/status
@@ -54,7 +60,11 @@ func patchNodeStatus(ctx context.Context, log logrus.FieldLogger, clientset kube
 			return nil
 		}
 		return err
-	}, defaultBackoff(ctx))
+	}, func(err error) {
+		log.Warnf("patch node status, will retry: %v", err)
+	})
+
+	err := wait.ExponentialBackoffWithContext(ctx, defaultBackoff(), patchNodeImpl)
 	if err != nil {
 		return fmt.Errorf("patch status: %w", err)
 	}
@@ -66,26 +76,30 @@ func getNodeForPatching(ctx context.Context, log logrus.FieldLogger, clientset k
 	// as a result was returned from watch. But subsequent get request returns not found.
 	// This is likely due to clientset's caching that's meant to alleviate API's load.
 	// So we give enough time for cache to sync.
-	logRetry := func(err error, _ time.Duration) {
-		log.Warnf("getting node, will retry: %v", err)
-	}
+
 	var node *v1.Node
-	b := backoff.NewExponentialBackOff()
-	b.MaxElapsedTime = 10 * time.Second
-	err := backoff.RetryNotify(func() error {
+	timeoutCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	getNodeImpl := waitext.WithTransientRetriesCtx(func(ctx context.Context) error {
 		var err error
 		node, err = clientset.CoreV1().Nodes().Get(ctx, nodeName, metav1.GetOptions{})
 		if err != nil {
 			return err
 		}
-		return err
-	}, b, logRetry)
+		return nil
+	}, func(err error) {
+		log.Warnf("getting node, will retry: %v", err)
+	})
+	boff := waitext.DefaultExponentialBackoff()
+
+	err := wait.ExponentialBackoffWithContext(timeoutCtx, boff, getNodeImpl)
 	if err != nil {
 		return nil, err
 	}
 	return node, nil
 }
 
-func defaultBackoff(ctx context.Context) backoff.BackOffContext {
-	return backoff.WithContext(backoff.WithMaxRetries(backoff.NewConstantBackOff(500*time.Millisecond), 5), ctx) // nolint:gomnd
+func defaultBackoff() wait.Backoff {
+	return waitext.WithRetry(waitext.NewConstantBackoff(500*time.Millisecond), 5)
 }
