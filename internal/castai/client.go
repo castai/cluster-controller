f5c87fc389f15b38fc7f8839ef67166c96a04aae
Valentyna Bukhalova <valentyna@cast.ai>
2024-10-16 12:21:37 +0200

    revert tls min version

--- internal/castai/client.go
+++ internal/castai/client.go
@@ -112,10 +112,9 @@ func createTLSConfig(ca string) (*tls.Config, error) {
 	if !certPool.AppendCertsFromPEM([]byte(ca)) {
 		return nil, fmt.Errorf("failed to add root certificate to CA pool")
 	}
-
+	//nolint:gosec
 	return &tls.Config{
 		RootCAs: certPool,
-		//nolint:gosec
 		//TODO set min version
 		// If the user did not configure a MinVersion and did not configure a
 		// MaxVersion < 1.2, use MinVersion=1.2, which is required by
