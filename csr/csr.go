0bbe45a766cefb833342865038badb01f16ba026
Marcin Kaciuba <aldor007@gmail.com>
2024-10-18 09:35:30 +0200

    fix: change log level to info (#144)

--- csr/csr.go
+++ csr/csr.go
@@ -302,7 +302,7 @@ func WatchCastAINodeCSRs(ctx context.Context, log logrus.FieldLogger, client kub
 
 	defer w.Stop()
 
-	log.Debug("watching for new node csr")
+	log.Info("watching for new node csr")
 
 	for {
 		select {
@@ -310,7 +310,7 @@ func WatchCastAINodeCSRs(ctx context.Context, log logrus.FieldLogger, client kub
 			return
 		case event, ok := <-w.ResultChan():
 			if !ok {
-				log.Debug("watcher closed")
+				log.Info("watcher closed")
 				go WatchCastAINodeCSRs(ctx, log, client, c) // start over in case of any error
 				return
 			}
@@ -323,7 +323,7 @@ func WatchCastAINodeCSRs(ctx context.Context, log logrus.FieldLogger, client kub
 				log.WithFields(logrus.Fields{
 					"csr":       name,
 					"node_name": csrResult.RequestingUser,
-				}).Debugf("skipping csr not from kubelet-bootstrap: %v", csrResult.RequestingUser)
+				}).Infof("skipping csr not from kubelet-bootstrap: %v", csrResult.RequestingUser)
 				continue
 			}
 
@@ -332,7 +332,7 @@ func WatchCastAINodeCSRs(ctx context.Context, log logrus.FieldLogger, client kub
 				log.WithFields(logrus.Fields{
 					"csr":       name,
 					"node_name": cn,
-				}).Debugf("skipping csr unable to get common name: %v", err)
+				}).Infof("skipping csr unable to get common name: %v", err)
 				continue
 			}
 			if csrResult.Approved() {
@@ -343,7 +343,7 @@ func WatchCastAINodeCSRs(ctx context.Context, log logrus.FieldLogger, client kub
 				log.WithFields(logrus.Fields{
 					"csr":       name,
 					"node_name": cn,
-				}).Debug("skipping csr not CAST AI node")
+				}).Infof("skipping csr not CAST AI node")
 				continue
 			}
 			csrResult.Name = cn
