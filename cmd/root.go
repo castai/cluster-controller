2b1c79cc0371e559d8364507cf58a884ecd5e0e0
Valentyna Bukhalova <valentyna@cast.ai>
2024-10-18 14:20:50 +0200

    use controller cmd by default

--- cmd/root.go
+++ cmd/root.go
@@ -15,6 +15,22 @@ var rootCmd = &cobra.Command{
 }
 
 func Execute(ctx context.Context) {
+	var cmdFound bool
+	cmd := rootCmd.Commands()
+
+	for _, a := range cmd {
+		for _, b := range os.Args[1:] {
+			if a.Name() == b {
+				cmdFound = true
+				break
+			}
+		}
+	}
+	if !cmdFound {
+		args := append([]string{controller.Use}, os.Args[1:]...)
+		rootCmd.SetArgs(args)
+	}
+
 	if err := rootCmd.ExecuteContext(ctx); err != nil {
 		fatal(err)
 	}
