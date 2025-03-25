c0629de9dbba9d2dd5c94f466a3063fede0a4baf
Valentyna Bukhalova <valentyna@cast.ai>
2025-03-25 13:42:19 +0100

    clean handler action

--- internal/actions/approve_csr_handler.go
+++ internal/actions/approve_csr_handler.go
@@ -2,35 +2,18 @@ package actions
 
 import (
 	"context"
-	"time"
-
-	"github.com/sirupsen/logrus"
-	"k8s.io/client-go/kubernetes"
-
 	"github.com/castai/cluster-controller/internal/castai"
 )
 
-const (
-	approveCSRTimeout = 4 * time.Minute
-)
+// TODO deprecated action
 
 var _ ActionHandler = &ApproveCSRHandler{}
 
-func NewApproveCSRHandler(log logrus.FieldLogger, clientset kubernetes.Interface) *ApproveCSRHandler {
-	return &ApproveCSRHandler{
-		log:                    log,
-		clientset:              clientset,
-		initialCSRFetchTimeout: 5 * time.Minute,
-		csrFetchInterval:       5 * time.Second,
-	}
+func NewApproveCSRHandler() *ApproveCSRHandler {
+	return &ApproveCSRHandler{}
 }
 
-type ApproveCSRHandler struct {
-	log                    logrus.FieldLogger
-	clientset              kubernetes.Interface
-	initialCSRFetchTimeout time.Duration
-	csrFetchInterval       time.Duration
-}
+type ApproveCSRHandler struct{}
 
 func (h *ApproveCSRHandler) Handle(_ context.Context, _ *castai.ClusterAction) error {
 	return nil
