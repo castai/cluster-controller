d26ceaa2b8035706d5686e13d4e87e7ee7020b39
Illia Litovchenko <150928945+cirisked@users.noreply.github.com>
2026-01-22 14:21:50 +0000

    CC load test improvements. (#214)

--- hack/loadtest/deploy.sh
+++ hack/loadtest/deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Optional values file can be passed as first argument
+VALUES_FILE="${1:-}"
+
 CC_IMAGE_REPOSITORY="${IMAGE_REPOSITORY:-us-docker.pkg.dev/castai-hub/library/cluster-controller}"
 CC_IMAGE_TAG="${IMAGE_TAG:-latest}"
 LOAD_TEST_IMAGE_REPOSITORY="${LOAD_TEST_IMAGE_REPOSITORY:-$CC_IMAGE_REPOSITORY}"
@@ -14,23 +17,35 @@ echo "Deploying kwok"
 helm repo add kwok https://kwok.sigs.k8s.io/charts/
 helm repo update kwok
 
-helm upgrade --namespace castai-agent --create-namespace  --install kwok kwok/kwok --set replicas="$KWOK_REPLICAS"
-helm upgrade --namespace castai-agent --create-namespace  --install kwok-stages kwok/stage-fast
-helm upgrade --namespace castai-agent --create-namespace  --install kwok-metrics kwok/metrics-usage
+helm upgrade --namespace castai-agent --create-namespace --install kwok kwok/kwok --set replicas="$KWOK_REPLICAS"
+helm upgrade --namespace castai-agent --create-namespace --install kwok-stages kwok/stage-fast
+helm upgrade --namespace castai-agent --create-namespace --install kwok-metrics kwok/metrics-usage
 
 if [ "$DEPLOY_CLUSTER_CONTROLLER" = "true" ]; then
   echo "Deploying cluster controller"
-  helm upgrade --namespace castai-agent --create-namespace --install  cluster-controller castai-helm/castai-cluster-controller \
-    --set castai.apiKey="dummy" \
-    --set castai.apiURL="http://castai-loadtest-agent-service.castai-agent.svc.cluster.local.:8080" \
-    --set castai.clusterID="00000000-0000-0000-0000-000000000000" \
-    --set image.repository="$CC_IMAGE_REPOSITORY" \
-    --set image.tag="$CC_IMAGE_TAG" \
-    --set image.pullPolicy="Always" \
+  HELM_ARGS=(
+    --namespace castai-agent
+    --create-namespace
+    --install cluster-controller
+    castai-helm/castai-cluster-controller
+    --set castai.apiKey="dummy"
+    --set castai.apiURL="http://castai-loadtest-agent-service.castai-agent.svc.cluster.local.:8080"
+    --set castai.clusterID="00000000-0000-0000-0000-000000000000"
+    --set image.repository="$CC_IMAGE_REPOSITORY"
+    --set image.tag="$CC_IMAGE_TAG"
+    --set image.pullPolicy="Always"
     --set autoscaling.enabled="true"
+  )
+
+  if [ -n "$VALUES_FILE" ]; then
+    echo "Using values file: $VALUES_FILE"
+    HELM_ARGS+=(--values "$VALUES_FILE")
+  fi
+
+  helm upgrade "${HELM_ARGS[@]}"
 fi
 
 echo "Deploying load testing components"
-kubectl kustomize "$SCRIPT_DIR" | \
-  LOADTEST_REPOSITORY="$LOAD_TEST_IMAGE_REPOSITORY" LOADTEST_TAG="$LOAD_TEST_IMAGE_TAG" envsubst \$LOADTEST_REPOSITORY,\$LOADTEST_TAG | \
+kubectl kustomize "$SCRIPT_DIR" |
+  LOADTEST_REPOSITORY="$LOAD_TEST_IMAGE_REPOSITORY" LOADTEST_TAG="$LOAD_TEST_IMAGE_TAG" envsubst \$LOADTEST_REPOSITORY,\$LOADTEST_TAG |
   kubectl apply -f -
