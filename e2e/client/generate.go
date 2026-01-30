1328482162b4f0e5f5cb6dbb050a4c8f901f4e22
Artūras Lapienė <arturas.lapiene@cast.ai>
2026-01-30 12:54:05 +0200

    update api-gen to v2

--- e2e/client/generate.go
+++ e2e/client/generate.go
@@ -1,5 +1,4 @@
 package client
 
-//go:generate go install github.com/deepmap/oapi-codegen/cmd/oapi-codegen@v1.11.0
-//go:generate oapi-codegen -o api.gen.go --old-config-style -generate types -include-tags $API_TAGS -package client $SWAGGER_LOCATION
-//go:generate oapi-codegen -o client.gen.go --old-config-style -templates codegen/templates -generate client -include-tags $API_TAGS -package client $SWAGGER_LOCATION
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -o api.gen.go --old-config-style -generate types -include-tags $API_TAGS -package client $SWAGGER_LOCATION
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -o client.gen.go --old-config-style -templates codegen/templates -generate client -include-tags $API_TAGS -package client $SWAGGER_LOCATION
